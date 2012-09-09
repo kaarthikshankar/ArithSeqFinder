d13c2c6f4fdb4d31a309b2cd88413237baa2e16b
Divya Ranjit <divroxalwz@gmail.com>
2012-09-09 05:13:56 -0400

    Multiple and Dedicated actors

--- ArithmaticSequenceFinder.scala
+++ ArithmaticSequenceFinder.scala
@@ -14,7 +14,7 @@ import scala.testing.Benchmark
  */
 
 case class ArithmaticSequence(first_num: Double, sequence_length: Int)
-case class Sum(first_num: Double, sequence_length: Int)
+case class Sum(first_num: Double, sequence_length: Int, comparator: Actor)
 case class Compare(first_num: Double, sum: Double)
 case class CompareResult(first_num: Double)
 
@@ -44,6 +44,7 @@ object ArithmaticSequenceFinder {
   def collect_values{
     println("Enter the maximum possible First Number : ");
     first_num = readDouble()
+    println("Number u entered : "+first_num)
     println("Enter the sequence length : ");
     sequence_length = readInt()
   }
@@ -56,14 +57,15 @@ object ArithmaticSequenceFinder {
 }
 
 object BossActor extends Actor{
-  var arr: Array[Double] = Array()
+  var slave_actors: Array[SlaveActor] = Array()
+  var compare_actors: Array[SlaveActor] = Array()
   var max_iterations: Double = -1
-  var calls_made = 0
+  var max_actors_needed = -1
+
   def act(){
     loop {
       react{
         case ArithmaticSequence(first_num, sequence_length) => process(first_num, sequence_length)
-        case CompareResult(first_num: Double) => store_result(first_num)
       }
     }
   }
@@ -71,13 +73,16 @@ object BossActor extends Actor{
   def process(first_num: Double, sequence_length: Int){
     println("In process");
     max_iterations = first_num
-    val sa1 = new SlaveActor
-    sa1.start()
+    create_actors
+    start_actors
+    DataDisplayActor.start()
+    DataDisplayActor.max_iterations = first_num
     //for(i <- 1 to first_num)
     try{
       var i: Double = 1
       while(true){
-        sa1 ! Sum(i,sequence_length)
+        var index = (i % max_actors_needed).toInt
+        slave_actors(index) ! Sum(i,sequence_length, compare_actors(index))
         if(i == first_num)
           throw Myexception
         i=i+1
@@ -88,7 +93,82 @@ object BossActor extends Actor{
     }
   }
 
+  def create_actors{
+    for (act <- 1 to 5){
+      slave_actors:+= new SlaveActor
+      compare_actors:+= new SlaveActor
+    }
+  }
+
+  def start_actors{
+    if (max_iterations <= slave_actors.length)
+      max_actors_needed = (max_iterations - 1).toInt
+    else
+    max_actors_needed =  slave_actors.length - 1
+    for (index <- 0 to max_actors_needed){
+      slave_actors(index).start()
+      compare_actors(index).start()
+    }
+  }
+}
+
+class SlaveActor extends Actor {
+  def act() {
+    loop {
+      react{
+        case Sum(first_num,sequence_length, comparator) => {square_and_add(first_num,sequence_length,comparator)}
+        case Compare(first_num: Double, sum: Double) => {compare(first_num,sum, sender)}
+      }
+    }
+  }
+  def square_and_add(first_num: Double,sequence_length: Int, comparator: Actor){
+    var sum:Double = 0
+    //for (i <- first_num to first_num+sequence_length-1)
+    try{
+      var i:Double = first_num
+      while(true){
+        sum += i*i
+        if (i==first_num+sequence_length-1)
+          throw Myexception
+        i=i+1
+
+      }
+    }
+    catch{
+      case Myexception => ()
+    }
+    comparator ! Compare(first_num, sum)
+  }
+  def compare(first_num: Double,sum: Double, from: OutputChannel[Any]){
+
+    val square_root: Double = Math.sqrt(sum)
+    //println("comparing "+ square_root)
+    if(Math.floor(square_root) == square_root)
+    {
+      DataDisplayActor ! CompareResult(first_num)
+      println("EQUAL COMPAREEEEEE")
+    }
+    else
+    {
+      DataDisplayActor ! CompareResult(-1)
+    }
+  }
+}
+
+object DataDisplayActor extends Actor{
+  var arr: Array[Double] = Array()
+  var max_iterations: Double = -1
+  var calls_made = 0
+  def act(){
+    loop {
+      react{
+        case CompareResult(first_num: Double) => store_result(first_num)
+      }
+    }
+  }
+
   def store_result(first_num: Double){
+    //println("I am in store result ! : "+first_num)
     calls_made += 1;
     if(first_num != -1)
     {
@@ -106,47 +186,5 @@ object BossActor extends Actor{
       println("CPU TIME : " + bean.getProcessCpuTime)
       System.exit(0)
     }
-  }
-}
-
-class SlaveActor extends Actor {
-  def act() {
-    loop {
-      react{
-        case Sum(first_num,sequence_length) => {square_and_add(first_num,sequence_length)}
-        case Compare(first_num: Double, sum: Double) => {compare(first_num,sum, sender)}
-      }
-    }
-  }
-  def square_and_add(first_num: Double,sequence_length: Int){
-    var sum:Double = 0
-    println("In sa")
-    //for (i <- first_num to first_num+sequence_length-1)
-    try{
-      var i:Double = first_num
-      while(true){
-        sum += i*i
-        if (i==first_num+sequence_length-1)
-          throw Myexception
-        i=i+1
-
-      }
-    }
-    catch{
-      case Myexception => ()
-    }
-
-
-    val sa2 = new SlaveActor
-    sa2.start()
-    sa2 ! Compare(first_num, sum)
-  }
-  def compare(first_num: Double,sum: Double, from: OutputChannel[Any]){
-    println("In cmpr " + first_num)
-    val square_root: Double = Math.sqrt(sum.doubleValue())
-    if(Math.floor(square_root) == square_root)
-      BossActor ! CompareResult(first_num)
-    else
-      BossActor ! CompareResult(-1)
   }
 }
