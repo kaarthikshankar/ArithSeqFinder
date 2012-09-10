d634afd2ffe2d1b1e43aa27e6786d1ef8231d9c9
kaarthikshankar Palraj <kaarthikshankar@gmail.com>
2012-09-10 02:24:36 -0400

    Scaled to e8 with Exit Logic

--- ArithmaticSequenceFinder.scala
+++ ArithmaticSequenceFinder.scala
@@ -17,6 +17,10 @@ case class ArithmaticSequence(first_num: Double, sequence_length: Int)
 case class Sum(first_num: Double, sequence_length: Int, comparator: Actor)
 case class Compare(first_num: Double, sum: Double)
 case class CompareResult(first_num: Double)
+case object Size
+case class DoneByBoss(count: Double, comparator: Actor)
+case class DoneByPeer(count: Double)
+case class DoneByComparator(load: Double)
 
 object Myexception extends Exception{
 }
@@ -26,10 +30,6 @@ object Systime{
   var cpu_time:Long = 0
 }
 
-object Actorpool{
-  var actor_array:Array[Actor] = Array()
-}
-
 object ArithmaticSequenceFinder {
 
   var first_num: Double = 0
@@ -37,7 +37,7 @@ object ArithmaticSequenceFinder {
   def main(args: Array[String]) {
     collect_values
     println("Sys "+ System.nanoTime())
-    Systime.time =  System.nanoTime();
+    Systime.time = System.nanoTime();
     start_master_and_send_val
   }
 
@@ -58,6 +58,7 @@ object ArithmaticSequenceFinder {
 
 object BossActor extends Actor{
   var slave_actors: Array[SlaveActor] = Array()
+  var load_per_actor: Array[Double] = Array()
   var compare_actors: Array[SlaveActor] = Array()
   var max_iterations: Double = -1
   var max_actors_needed = -1
@@ -80,13 +81,32 @@ object BossActor extends Actor{
     //for(i <- 1 to first_num)
     try{
       var i: Double = 1
+      var now_sent: Double = 0
+      var tired_recurs = 1
       while(true){
         var index = (i % max_actors_needed).toInt
         slave_actors(index) ! Sum(i,sequence_length, compare_actors(index))
-        if(i == first_num)
+        load_per_actor(index) += 1
+        now_sent += 1
+        if(now_sent >= 1e7){
+          println("___Tired___")
+          Thread.sleep(40000)
+          now_sent = 0
+          tired_recurs +=1
+          println("___Woke Up__")
+        }
+        //if (index == 1) slave_actors(index) ! Size
+        if(i == first_num){
+          println("Actors size" + slave_actors.size)
+          for (index <- load_per_actor){
+            println("Load Per Actors "+ index)
+          }
+          for (index <- 0 to slave_actors.length - 1)
+            slave_actors(index) ! DoneByBoss(load_per_actor(index), compare_actors(index))
           throw Myexception
-        i=i+1
       }
+        i=i+1
+     }
     }
     catch{
       case Myexception => ()
@@ -94,18 +114,19 @@ object BossActor extends Actor{
   }
 
   def create_actors{
-    for (act <- 1 to 5){
+    for (act <- 1 to 4){
       slave_actors:+= new SlaveActor
       compare_actors:+= new SlaveActor
+      load_per_actor:+= 0.0
     }
   }
 
   def start_actors{
     if (max_iterations <= slave_actors.length)
-      max_actors_needed = (max_iterations - 1).toInt
+      max_actors_needed = (max_iterations).toInt
     else
-    max_actors_needed =  slave_actors.length - 1
-    for (index <- 0 to max_actors_needed){
+      max_actors_needed = slave_actors.length
+    for (index <- 0 to max_actors_needed - 1){
       slave_actors(index).start()
       compare_actors(index).start()
     }
@@ -113,17 +134,20 @@ object BossActor extends Actor{
 }
 
 class SlaveActor extends Actor {
+  var my_load: Double = 0
   def act() {
     loop {
       react{
         case Sum(first_num,sequence_length, comparator) => {square_and_add(first_num,sequence_length,comparator)}
+        case Size => println("Mail Box Size " + mailboxSize);
         case Compare(first_num: Double, sum: Double) => {compare(first_num,sum, sender)}
+        case DoneByBoss(load: Double, comparator: Actor) => {my_load = load; comparator ! DoneByPeer(my_load)}
+        case DoneByPeer(load: Double) => DataDisplayActor ! DoneByComparator(load)
       }
     }
   }
   def square_and_add(first_num: Double,sequence_length: Int, comparator: Actor){
     var sum:Double = 0
-    //for (i <- first_num to first_num+sequence_length-1)
     try{
       var i:Double = first_num
       while(true){
@@ -138,19 +162,15 @@ class SlaveActor extends Actor {
       case Myexception => ()
     }
     comparator ! Compare(first_num, sum)
+    if(my_load > 0) {println("Done by boss");comparator ! DoneByPeer(my_load)}
   }
+
   def compare(first_num: Double,sum: Double, from: OutputChannel[Any]){
 
     val square_root: Double = Math.sqrt(sum)
-    //println("comparing "+ square_root)
     if(Math.floor(square_root) == square_root)
     {
       DataDisplayActor ! CompareResult(first_num)
-      println("EQUAL COMPAREEEEEE")
-    }
-    else
-    {
-      DataDisplayActor ! CompareResult(-1)
     }
   }
 }
@@ -158,18 +178,26 @@ class SlaveActor extends Actor {
 object DataDisplayActor extends Actor{
   var arr: Array[Double] = Array()
   var max_iterations: Double = -1
-  var calls_made = 0
+  var unequals:Int = 0
+  var equals:Int = 0
+  var calls_made: Double = 0
   def act(){
     loop {
       react{
         case CompareResult(first_num: Double) => store_result(first_num)
+        case DoneByComparator(load: Double) => calls_made += load; if (calls_made == max_iterations){
+          println("Final Output")
+          for (i <- arr)
+            println(i)
+          System.exit(0);
+        }
       }
     }
   }
 
   def store_result(first_num: Double){
-    //println("I am in store result ! : "+first_num)
-    calls_made += 1;
+    //println("Calls Made : "+calls_made)
+    //println("Max Iterations : "+max_iterations)
     if(first_num != -1)
     {
       println("Number : "+first_num)
